af2c34cd1ec5c550954b2eca0d7645f2c8fdb424
WendyXpenguin <yuxinxupenguin@gmail.com>
2023-06-09 11:23:18 -0400

    Initial Commit

---
+++ P2P_Bluetooth/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  P2P_Bluetooth
+//
+//  Created by LostZ on 2023/6/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ P2P_Bluetooth/P2P_BluetoothApp.swift
@@ -0,0 +1,17 @@
+//
+//  P2P_BluetoothApp.swift
+//  P2P_Bluetooth
+//
+//  Created by LostZ on 2023/6/9.
+//
+
+import SwiftUI
+
+@main
+struct P2P_BluetoothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
