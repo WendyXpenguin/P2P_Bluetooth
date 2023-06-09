92e1ab2ea185731998447b9f8415e134f4f9b153
WendyXpenguin <yuxinxupenguin@gmail.com>
2023-06-09 11:32:14 -0400

    second commit, test

--- P2P_Bluetooth/ContentView.swift
+++ P2P_Bluetooth/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, how are you!!")
+            Text("Hello, what a good day!")
         }
         .padding()
     }
