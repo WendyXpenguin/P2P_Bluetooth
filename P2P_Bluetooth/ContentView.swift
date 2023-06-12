5a330da901628fd9f6f30d22e3ab2baaeb01438e
WendyXpenguin <yuxinxupenguin@gmail.com>
2023-06-12 12:01:04 -0400

    6/12 12:00 pm

--- P2P_Bluetooth/ContentView.swift
+++ P2P_Bluetooth/ContentView.swift
@@ -8,28 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var messageText = ""
+    @State var messages: [String] = ["Let's chat!"]
     var body: some View {
         VStack{
-            textBoxView()
+            HStack{
+                Text("UserName")
+                    .font(.largeTitle)
+                    .bold()
+                Image(systemName: "text.bubble.fill")
+                    .font(.system(size: 26))
+                    .foregroundColor(Color.mint)
+            }
+            ScrollView{
+                ForEach(messages, id: \.self) {
+                    message in Text(message)
+                }
+            }
+            HStack{
+                TextField("Type your message here", text : $messageText)
+                    .padding()
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(10)
+                    .onSubmit {
+                        sendMessage(message: messageText)
+                    }
+                Button {
+                    sendMessage(message: messageText)
+                } label : {
+                    Image(systemName: "paperplane.fill")
+                }
+                .font(.system(size:26))
+                .padding(.horizontal, 10)
+                .foregroundColor(Color.mint)
+            }
+            .padding()
         }
     }
-}
-
-struct textBoxView: View {
-    var body: some View {
-        ZStack(alignment:.center){
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(lineWidth: 5)
+    func sendMessage(message: String){
+        withAnimation{
+            messages.append("[USER]" + message)
+            self.messageText = ""
         }
-        .frame(width: 270, height: 570.0)
-        .foregroundColor(Color.blue)
-        .padding([.leading, .bottom, .trailing], 40.0)
     }
 }
 
 // two maps to handle the user names and IDs
-
-
 
 
 struct ContentView_Previews: PreviewProvider {
