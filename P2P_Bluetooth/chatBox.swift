b428c24bb75e5dbdecc261bd1b9c1aeb1e0b4c52
WendyXpenguin <yuxinxupenguin@gmail.com>
2023-06-13 13:52:58 -0400

    commit for new file for the UUID

--- P2P_Bluetooth/chatBox.swift
+++
@@ -1,8 +0,0 @@
-//
-//  chatBox.swift
-//  P2P_Bluetooth
-//
-//  Created by LostZ on 2023/6/9.
-//
-
-import Foundation
---
+++ P2P_Bluetooth/uniqueID.swift
@@ -0,0 +1,13 @@
+//
+//  uniqueID.swift
+//  P2P_Bluetooth
+//
+//  Created by LostZ on 2023/6/13.
+//
+
+import Foundation
+
+struct userNames {
+    let id = UUID().uuidString
+    let username: String
+}
